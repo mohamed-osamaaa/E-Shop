9d7ede35a97478d6354d6282b335fdf1a694e94c
Mohamed Osama <141563962+mohamed-osamaaa@users.noreply.github.com>
2025-02-19 08:57:12 +0200

    commit1

--- src/components/NabBar.jsx
+++ src/components/NabBar.jsx
@@ -1,17 +1,33 @@
 import { useState } from 'react';
 
+import {
+    Search,
+    ShoppingCart,
+} from 'lucide-react';
+
 function NabBar() {
     const [open, setOpen] = useState(false);
+    const [dark, setDark] = useState(false);
 
     return (
-        <div>
+        <div className="grid grid-cols-2 gap-14">
             <div>
-                <h1>ESHOP</h1>
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#Shop">Shop</a></li>
-                    <li><a href="#About">About</a></li>
-                    <li><a href="#Blogs">Blogs</a></li>
+                <ul className='flex justify-center items-center space-x-4'>
+                    <li>
+                        ESHOP
+                    </li>
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#Shop">Shop</a>
+                    </li>
+                    <li>
+                        <a href="#About">About</a>
+                    </li>
+                    <li>
+                        <a href="#Blogs">Blogs</a>
+                    </li>
 
                     <li
                         onMouseEnter={() => setOpen(true)}
@@ -19,19 +35,46 @@ function NabBar() {
                     >
                         <button>
                             Quick Links
-                            <span className={`transform transition-transform ${open ? "rotate-180" : "rotate-0"}`}>
+                            <span
+                                className={`transform transition-transform ${open ? "rotate-180" : "rotate-0"
+                                    }`}
+                            >
                                 ▼
                             </span>
                         </button>
                         {open && (
                             <ul>
-                                <li><a href="#TrendingProducts">Trending Products</a></li>
-                                <li><a href="#BestSelling">Best Selling</a></li>
-                                <li><a href="#TopRated">Top Rated</a></li>
+                                <li>
+                                    <a href="#TrendingProducts">
+                                        Trending Products
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#BestSelling">Best Selling</a>
+                                </li>
+                                <li>
+                                    <a href="#TopRated">Top Rated</a>
+                                </li>
                             </ul>
                         )}
                     </li>
                 </ul>
+            </div>
+            <div className='flex justify-end items-center space-x-4'>
+                <Search />
+                <div>
+                    <ShoppingCart />
+                </div>
+                <div onClick={() => setDark((prev) => !prev)}>
+                    <img
+                        src={
+                            dark
+                                ? "../assets/website/dark-mode-button.png"
+                                : "../assets/website/light-mode-button.png"
+                        }
+                        alt={dark ? "Dark" : "Light"}
+                    />
+                </div>
             </div>
         </div>
     );
